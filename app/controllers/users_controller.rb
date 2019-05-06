f01bcf101d92400bb989c3464dd034f39cf87951
Sharon Alajajian <sharonoptsout@gmail.com>
2019-05-06 13:25:37 -0400

    update user controller show, user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     @user.save
-    #session[:user_id] = @user.id
     log_in(@user)
     ##binding.pry
     redirect_to root_path
@@ -29,6 +28,13 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(params[:id])
+    if logged_in? && @user == current_user
+      render 'show'
+    else
+      flash[:notice] = "Not authorized to see that page"
+      redirect_to root_path
+    end
   end
 
   private
