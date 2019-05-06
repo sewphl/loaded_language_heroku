292c59bd82bfef504464b7b2d86ec23557ba9693
Sharon Alajajian <sharonoptsout@gmail.com>
2019-05-06 09:52:19 -0400

    generate sessions controller, add routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,14 +12,14 @@ class UsersController < ApplicationController
     @user.save
     session[:user_id] = @user.id
     redirect_to root_path
-    ##
-    #  redirect_to new_users_path, alert: "please retry signing up"
-
   end
 
   def edit
-    @user = User.find(params[:id])
-    ##add something in case user not found, flash message and redirect.
+    if logged_in?
+      @user = current_user
+    else
+      redirect_to new_users_path
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #get 'sessions/new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   get 'welcome/index' => 'welcome#index'
   root 'script#index'
   resources :users do
