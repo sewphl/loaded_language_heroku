2b978ef972e58dc030eccd18ee022bc2c839bf8c
Sharon Alajajian <sharonoptsout@gmail.com>
2019-05-07 13:25:29 -0400

    update words controller (nested), update word routes, update word views, user views, create migration to add user_id attribute to word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,7 @@
 class WordsController < ApplicationController
 
   def index
+
     @words = Word.all
   end
 
@@ -9,7 +10,7 @@ class WordsController < ApplicationController
   end
 
   def new
-    @word = Word.new
+    @word = Word.new(user_id: params[:user_id])
   end
 
   def create
@@ -18,7 +19,17 @@ class WordsController < ApplicationController
   end
 
   def edit
-    @word = Word.find_by(id: params[:id])
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user.nil?
+        redirect_to users_path, alert: "User not found."
+      else
+        @word = user.words.find_by(id: params[:id])
+        redirect_to user_words_path(user), alert: "Word not found." if @word.nil?
+      end
+    else
+      @word = Word.find(params[:id])
+    end
   end
 
   def update
@@ -30,7 +41,7 @@ class WordsController < ApplicationController
   private
 
   def word_params
-    params.require(:word).permit(:entry, :loaded_entry)
+    params.require(:word).permit(:entry, :loaded_entry, :user_id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_154816) do
+ActiveRecord::Schema.define(version: 2019_05_07_172245) do
 
   create_table "feelings", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_05_03_154816) do
     t.float "loaded_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
 end
