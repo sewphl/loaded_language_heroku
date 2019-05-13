809c3910dcd794df78525fdf49cb59f98cf60990
Sharon Alajajian <sharonoptsout@gmail.com>
2019-05-13 17:13:48 -0400

    create word method to compute average of all feelings, individual feelings

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,9 +5,14 @@ class Word < ApplicationRecord
   accepts_nested_attributes_for :word_feelings
   validates_uniqueness_of :entry, :message=>"that word already exists"
 
-
-  def compute_avg_loaded(word)
-    mywords = WordFeeling.where(word_id: word.id)
+  def compute_avg(word,feeling=nil)
+    if feeling !=nil
+      ##compute average OF ALL FEELINGS FOR A SINGLE WORD.
+      mywords = WordFeeling.where(word_id: word.id, feeling_id: feeling.id)
+    else
+      ##compute average FOR A SINGLE FEELING FOR A SINGLE WORD.
+      mywords = WordFeeling.where(word_id: word.id)
+    end
     mywords.average(:feeling_rating).round(2)
   end
 
