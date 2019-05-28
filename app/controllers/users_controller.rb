46c364ec276cd7a44c83030b77b5baee356280d6
Sharon Alajajian <sharonoptsout@gmail.com>
2019-05-28 17:09:28 -0400

    update view or edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
     if logged_in? && @user == current_user
       render 'show'
     else
