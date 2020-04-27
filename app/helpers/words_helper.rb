9a0c3f6463c23abac976b44670d8dc52319348ce
sewphl <sharonoptsout@gmail.com>
2020-04-27 13:55:45 -0400

    update and add new words helper methods

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -8,15 +8,24 @@ module WordsHelper
     word.created_at.strftime("%I:%M %p")
   end
 
-  def compute_avg(word,feeling=nil)
-    if feeling !=nil
-      ##compute average OF ALL FEELINGS FOR A SINGLE WORD.
-      myword = WordFeeling.where(word_id: word.id, feeling_id: feeling.id)
-    else
-      ##compute average FOR A SINGLE FEELING FOR A SINGLE WORD.
-      myword = WordFeeling.where(word_id: word.id)
-    end
+  def compute_avg(word,feeling)
+    ##compute average FOR A SINGLE FEELING FOR A SINGLE WORD.
+    myword = WordFeeling.where(word_id: word.id, feeling_id: feeling.id)
     myword.average(:feeling_rating).round(2)
+  end
+
+  def compute_median(word,feeling)
+    ##find median FOR A SINGLE FEELING FOR A SINGLE WORD.
+    myword = WordFeeling.where(word_id: word.id, feeling_id: feeling.id)
+    rating_arr = myword.pluck(:feeling_rating)
+    sorted = rating_arr.sort
+    len = sorted.length
+    (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+  end
+
+  ##checks to see if current user has already ranked a word in db.
+  def already_ranked?(word,user)
+    WordFeeling.where(word_id: word.id, user_id: user.id).exists?
   end
 
 
